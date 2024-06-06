1873c0622a71d6852f59605a67777765b2908bf5
Taran Mesala <meesalataran58@gmail.com>
2024-06-06 16:32:05 +0530

    Weather Report created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder,ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,CommonModule,HttpClientModule],
+  providers: [WeatherService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Assignment';
+export class AppComponent implements OnInit {
+  title = 'Weather';
+  weatherData: any;
+  errorMessage: string ='';
+  myForm: FormGroup = new FormGroup({});
+  constructor(private formBuilder:FormBuilder, private weatherService: WeatherService){}
+  ngOnInit(): void {
+      this.myForm = this.formBuilder.group({
+        city:['',Validators.required]
+      });
+  }
+  formSubmit(){
+    const cityName = this.myForm.get('city')?.value;
+    console.log(cityName);
+    this.weatherService.getWeather(cityName).subscribe(
+      (data: any) => {
+        this.weatherData = data;
+        this.errorMessage ="";
+      },
+      (error: any )=> {
+        this.errorMessage = 'Could not fetch weather data';
+        this.weatherData = null;
+      }
+    ); 
+  }
+  moveContainer() {
+    const container = document.getElementById("cont") as HTMLDivElement | null;
+    if (container !== null) {
+      container.style.marginTop = "10px";
+    } else {
+      alert("Element with ID 'container' not found.");
+    }
+  }
+  
+  
+ } 
+interface weatherData {
+  name: string; 
+  main: {
+    temp: number;
+    humidity: number; 
+  };
+  weather: {
+    description: string; 
+  }[];
 }
---
+++ src/app/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey: string = '941224ddc1f5c7c575d108f1ece06957';
+  private apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&APPID=${this.apiKey}&units=metric`;
+    return this.http.get(url);
+  }
+}
